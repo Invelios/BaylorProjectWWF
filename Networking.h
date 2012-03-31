5865d85da408787c72d02884b7a7574a29443344
Christopher_Scoggins <Christopher_Scoggins@baylor.edu>
2012-03-30 21:43:09 -0500

    Got frame for Networking written. Not implemented yet. Wwf files for networking stream.

---
+++ Networking.h
@@ -0,0 +1,48 @@
+// Title    : Networking Implementation
+// Author   : Christopher Scoggins
+// Created  : 3/30/12
+// Last Mod : 3/30/12
+//
+// Implements Networking Class Functions
+
+#ifndef NETWORKING_H_INCLUDED
+#define NETWORKING_H_INCLUDED
+
+#include "Wwf.h"
+#include <vector>
+
+using namespace std;
+
+class Networking
+{
+    private:
+
+    wwfstream net;
+
+    public:
+
+    bool createAccount(String user, String pass, String email);
+
+    // User getAllUsers();
+
+    bool login(String user, String pass);
+
+    bool changePassword(String oldPass, String newPass);
+
+    int createGame(String user1, String user2);
+
+    vector<int> getAllGameID();
+
+    vector<int> getActiveGameID();
+
+    bool setGameStatus(int gameID, bool status);
+
+    // GameObject getGame(int gameID);
+
+    // bool updateGame(int gameID, GameObject, turn);
+
+    int getTurn(int gameID);
+
+}
+
+#endif // NETWORKING_H_INCLUDED

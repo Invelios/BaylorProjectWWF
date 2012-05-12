ae0f5de5c5c7accb89e8b0805902ff99d6b34c82
Christopher_Scoggins <Christopher_Scoggins@baylor.edu>
2012-05-12 14:17:06 -0500

    Moved files into an Eclipse Project

--- Globals.cpp
+++
@@ -1,9 +0,0 @@
-#include "Globals.h"
-
-/*using namespace std;
-
-const SDL_Surface * requestName = NULL;
-const SDL_Surface * loginBackground = NULL;
-const SDL_Surface * requestPass = NULL;
-const SDL_Surface * requestEmail = NULL;
-*/
